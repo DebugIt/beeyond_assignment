41a87fed32b604e532a23648d4ebd5857fbe7ae6
DebugIt <ronakmithiya@gmail.com>
2025-11-14 13:08:45 +0530

    lax dala hai

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -61,7 +61,7 @@ module.exports.login = async (req, res, next) => {
                 res.cookie('session', getToken, {
                     httpOnly: true,
                     secure: isProduction,
-                    sameSite: isProduction ? 'none' : 'lax',
+                    sameSite: 'lax',
                     maxAge: 7 * 24 * 60 * 60 * 1000
                 })
                 res.status(200).json({
