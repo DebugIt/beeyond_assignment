daf5eb1301aa850e9e2ab2abb019e69d22822375
DebugIt <ronakmithiya@gmail.com>
2025-11-14 12:53:52 +0530

    if u are seeing my commits get me hired broo!!!

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const socketio = new Server(server, {
 })
 
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', "http://app.apaarrethnic.com"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"]
 }))
